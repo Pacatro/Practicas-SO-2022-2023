e6e29146bc7689f685d692096867a82830794150
Pacatro <paco.algar.15@gmail.com>
2022-09-22 15:03:55 +0200

    No funciona bien el ej3 :(

--- p1/ej3/main.c
+++ p1/ej3/main.c
@@ -6,20 +6,10 @@
 #include <unistd.h>
 #include <string.h>
 
-int main(int argc, char **argv){
-    /*
-    if(argc != 1){
-        printf("You must write the name of the executables and the files name: (./program.out 'executable1' 'executable2' 'file1.txt', 'file2.txt'...)\n");
-        exit(EXIT_FAILURE);
-    }
-    */
+void child1(pid_t childpid, pid_t flag, int status, char **argv){
+    childpid = fork();
 
-    pid_t childpid_1, flag, childpid_2;
-    int status;
-
-    childpid_1 = fork();
-
-    switch(childpid_1){
+    switch(childpid){
         case -1:
             perror("ERROR, CAN'T CREATE CHILD PROCESS\n");
             printf("errno value = %d\n", errno);
@@ -39,11 +29,11 @@ int main(int argc, char **argv){
 
             while((flag = wait(&status)) > 0){
                 if(WIFEXITED(status)){
-                    printf("Father PID: %d, child with PID %d finished, status = %d\n", getpid(), childpid_1, WEXITSTATUS(status));
+                    printf("Father PID: %d, child with PID %d finished, status = %d\n", getpid(), childpid, WEXITSTATUS(status));
                 }
 
                 else if(WIFSIGNALED(status)){
-                    printf("Father PID: %d, child with PID %d finished at recibe signal, status = %d\n", getpid(), childpid_1, WTERMSIG(status));
+                    printf("Father PID: %d, child with PID %d finished at recibe signal, status = %d\n", getpid(), childpid, WTERMSIG(status));
                 }
             }
 
@@ -53,7 +43,66 @@ int main(int argc, char **argv){
                 printf("ERROR. CAN'T INVOQUE wait() or waitpid(), errno = %d -> %s\n", errno, strerror(errno));
                 exit(EXIT_FAILURE);
             }       
-    } //switch
+    }
+}
 
+void child2(pid_t childpid, pid_t flag, int status, char **argv){
+    childpid = fork();
+    switch(childpid){
+        case -1:
+            perror("ERROR, CAN'T CREATE CHILD PROCESS\n");
+            printf("errno value = %d\n", errno);
+        exit(EXIT_FAILURE);
+
+        case 0:
+            printf("I'm the child number 1, PID: %d, PPID: %d. Running calculator...\n", getpid(), getppid());
+
+            if(execlp(argv[2], argv[3], argv[4]) == -1){
+                perror("Can't excute the second order");
+                printf("errno value = %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+
+        default:
+            printf("I'm the father with PID: %d\n", getpid());
+
+            while((flag = wait(&status)) > 0){
+                if(WIFEXITED(status)){
+                    printf("Father PID: %d, child with PID %d finished, status = %d\n", getpid(), childpid, WEXITSTATUS(status));
+                }
+
+                else if(WIFSIGNALED(status)){
+                    printf("Father PID: %d, child with PID %d finished at recibe signal, status = %d\n", getpid(), childpid, WTERMSIG(status));
+                }
+            }
+
+            if(flag == (pid_t)-1 && errno == ECHILD){
+                printf("Process %d, no more childs to wait, errno = %d -> %s\n", getpid(), errno, strerror(errno));
+            } else{
+                printf("ERROR. CAN'T INVOQUE wait() or waitpid(), errno = %d -> %s\n", errno, strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+    }
+}
+
+int main(int argc, char **argv){
+    /*
+    if(argc != 1){
+        printf("You must write the name of the executables and the files name: (./program.out 'executable1' 'executable2' 'file1.txt', 'file2.txt'...)\n");
+        exit(EXIT_FAILURE);
+    }
+    */
+
+    pid_t childpid, flag;
+    int status;
+
+    printf("Running child 1\n");
+    child1(childpid, flag, status, argv);
+    
+    sleep(10);
+
+    printf("\nRunning child 2\n");
+    child2(childpid, flag, status, argv);
+    
     exit(EXIT_SUCCESS);
 }
