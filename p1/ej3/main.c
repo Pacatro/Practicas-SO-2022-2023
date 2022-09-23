689aecbe3fa562fc43808787c692cd0f53a1b706
Pacatro <paco.algar.15@gmail.com>
2022-09-23 13:21:59 +0200

    ej3 casi acabado

--- p1/ej3/main.c
+++ p1/ej3/main.c
@@ -6,82 +6,19 @@
 #include <unistd.h>
 #include <string.h>
 
-void child1(pid_t childpid, pid_t flag, int status, char **argv){
-    childpid = fork();
-
-    switch(childpid){
-        case -1:
-            perror("ERROR, CAN'T CREATE CHILD PROCESS\n");
-            printf("errno value = %d\n", errno);
+void child1(char **argv){
+    if(system(argv[1]) == -1){
+        perror("Can't excute calculator");
+        printf("errno value = %d\n", errno);
         exit(EXIT_FAILURE);
-
-        case 0:
-            printf("I'm the child number 1, PID: %d, PPID: %d. Running calculator...\n", getpid(), getppid());
-
-            if(system(argv[1]) == -1){
-                perror("Can't excute calculator");
-                printf("errno value = %d\n", errno);
-                exit(EXIT_FAILURE);
-            }
-
-        default:
-            printf("I'm the father with PID: %d\n", getpid());
-
-            while((flag = wait(&status)) > 0){
-                if(WIFEXITED(status)){
-                    printf("Father PID: %d, child with PID %d finished, status = %d\n", getpid(), childpid, WEXITSTATUS(status));
-                }
-
-                else if(WIFSIGNALED(status)){
-                    printf("Father PID: %d, child with PID %d finished at recibe signal, status = %d\n", getpid(), childpid, WTERMSIG(status));
-                }
-            }
-
-            if(flag == (pid_t)-1 && errno == ECHILD){
-                printf("Process %d, no more childs to wait, errno = %d -> %s\n", getpid(), errno, strerror(errno));
-            } else{
-                printf("ERROR. CAN'T INVOQUE wait() or waitpid(), errno = %d -> %s\n", errno, strerror(errno));
-                exit(EXIT_FAILURE);
-            }       
     }
 }
 
-void child2(pid_t childpid, pid_t flag, int status, char **argv){
-    childpid = fork();
-    switch(childpid){
-        case -1:
-            perror("ERROR, CAN'T CREATE CHILD PROCESS\n");
-            printf("errno value = %d\n", errno);
+void child2(char **argv){
+    if(execvp(argv[2], &argv[2]) == -1 ){
+        perror("Can't excute gedit");
+        printf("errno value = %d\n", errno);
         exit(EXIT_FAILURE);
-
-        case 0:
-            printf("I'm the child number 1, PID: %d, PPID: %d. Running calculator...\n", getpid(), getppid());
-
-            if(execlp(argv[2], argv[3], argv[4]) == -1){
-                perror("Can't excute the second order");
-                printf("errno value = %d\n", errno);
-                exit(EXIT_FAILURE);
-            }
-
-        default:
-            printf("I'm the father with PID: %d\n", getpid());
-
-            while((flag = wait(&status)) > 0){
-                if(WIFEXITED(status)){
-                    printf("Father PID: %d, child with PID %d finished, status = %d\n", getpid(), childpid, WEXITSTATUS(status));
-                }
-
-                else if(WIFSIGNALED(status)){
-                    printf("Father PID: %d, child with PID %d finished at recibe signal, status = %d\n", getpid(), childpid, WTERMSIG(status));
-                }
-            }
-
-            if(flag == (pid_t)-1 && errno == ECHILD){
-                printf("Process %d, no more childs to wait, errno = %d -> %s\n", getpid(), errno, strerror(errno));
-            } else{
-                printf("ERROR. CAN'T INVOQUE wait() or waitpid(), errno = %d -> %s\n", errno, strerror(errno));
-                exit(EXIT_FAILURE);
-            }
     }
 }
 
@@ -94,15 +31,45 @@ int main(int argc, char **argv){
     */
 
     pid_t childpid, flag;
-    int status;
+    int status, i;
 
-    printf("Running child 1\n");
-    child1(childpid, flag, status, argv);
+    for(i = 0; i<2; i++){
+        childpid = fork();
+
+        switch(childpid){
+            case -1:
+                perror("ERROR, CAN'T CREATE CHILD PROCESS\n");
+                printf("errno value = %d\n", errno);
+            exit(EXIT_FAILURE);
+
+            case 0:
+                printf("I'm the child, PID: %d, PPID: %d.\n", getpid(), getppid());
+
+                child1(argv);
+                child2(argv);
+
+            default:
+                printf("I'm the father with PID: %d\n", getpid());
+        
+        }
+    }
     
-    sleep(10);
+    while((flag = wait(&status)) > 0){
+        if(WIFEXITED(status)){
+            printf("Father PID: %d, child with PID %d finished, status = %d\n", getpid(), childpid, WEXITSTATUS(status));
+        }
 
-    printf("\nRunning child 2\n");
-    child2(childpid, flag, status, argv);
+        else if(WIFSIGNALED(status)){
+            printf("Father PID: %d, child with PID %d finished at recibe signal, status = %d\n", getpid(), childpid, WTERMSIG(status));
+        }
+    }
+
+    if(flag == (pid_t)-1 && errno == ECHILD){
+        printf("Process %d, no more childs to wait, errno = %d -> %s\n", getpid(), errno, strerror(errno));
+    } else{
+        printf("ERROR. CAN'T INVOQUE wait() or waitpid(), errno = %d -> %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
     
     exit(EXIT_SUCCESS);
 }
