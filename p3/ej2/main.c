87081afb7c874141cc8decb9aff0c985669f49d3
Pacatro <paco.algar.15@gmail.com>
2022-11-29 21:18:35 +0100

    EJ2 ACABADO

--- p3/ej2/main.c
+++ p3/ej2/main.c
@@ -11,12 +11,11 @@ pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 //------------ADITIONAL------------
 void nArray(int n, int *array);
 void nRandomArray(int n, int *array);
-int isPar(int n, int *array);
 //-----------------------------------
 
 void *add(void *value){
-    int index = *(int*) value;
-    int *randoms;
+    int index = *(int*) value, randoms[5], *total = (int *)malloc(sizeof(int)), i, j;
+    *total = 0;
 
     printf("Thread %lu, with index %d\n", pthread_self(), index);
 
@@ -28,10 +27,26 @@ void *add(void *value){
     nRandomArray(5, randoms);
 
     if(index % 2 == 0){
+        printf("Adding pares....\n");        
+        for(i = 0; i < 5; i++){
+            par += randoms[i];
+        }
 
+    } else{
+        printf("Adding impares....\n");
+        for(j = 0; j < 5; j++){
+            impar += randoms[j];
+        }
     }
 
-    pthread_exit(NULL);
+    *total += par + impar;
+
+    if(pthread_mutex_unlock(&mtx) != 0){
+        printf("Mutex_unlock error...\n");
+        pthread_exit(NULL);
+    }
+
+    pthread_exit((void*) total);
 }
 
 int main(int argc, char **argv){
@@ -41,7 +56,8 @@ int main(int argc, char **argv){
         exit(EXIT_FAILURE);
     }
 
-    int n = atoi(argv[1]), values[n];
+    int n = atoi(argv[1]), values[n], total_int = 0;
+    void *total;
     pthread_t threads[n];
 
     nArray(n, values);
@@ -55,18 +71,24 @@ int main(int argc, char **argv){
     }
 
     for(int i = 0; i<n; i++){
-        if(pthread_join(threads[i], NULL)){
+        if(pthread_join(threads[i], &total)){
             perror("Can't create clients threads");
             printf("errno = %d", errno);
             exit(EXIT_FAILURE);
         }
+
+        total_int += *(int*)total;
     }
+
+    printf("\nPar = %d\nImpar = %d\nTotal = %d\n", par, impar, *(int*)total);
+
+    printf("\nGlobal total = %d\n", total_int);
 
     exit(EXIT_SUCCESS);
 }
 
 void nArray(int n, int *array){
-    for(int i = 0; i<n; i++){
+    for(int i = 0; i < n; i++){
         array[i] = i+1;
     }
 }
@@ -74,7 +96,7 @@ void nArray(int n, int *array){
 void nRandomArray(int n, int *array){
     srand(time(NULL));
     
-    for(int i = 0; i<n; i++){
+    for(int i = 0; i < n; i++){
         array[i] = rand() % 11;
     }
 }
